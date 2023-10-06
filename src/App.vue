a08185b9cf4f9d480cbd9ddc830b9daaf716e658
Fahmedo <faruqahmedolaitan@gmail.com>
2023-10-06 23:54:51 +0100

    white line fixed

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HomeView from "./views/HomeView.vue";
-import AppNavbar from "./views/navbar.vue";
+import { RouterLink, RouterView } from 'vue-router';
+import HomeView from './views/HomeView.vue';
+import AppNavbar from './views/Navbar.vue';
 </script>
 
 <template>
-  <div class="w-screen h-[100vh] bg-[url(https://img.freepik.com/premium-photo/blue-bokeh-background-element-designnatura-blue-blurred-background_49507-3311.jpg)] bg-cover bg-no-repeat overflow-hidden">
+  <div
+    class="w-[100%] h-[100vh] bg-[url(https://img.freepik.com/premium-photo/blue-bokeh-background-element-designnatura-blue-blurred-background_49507-3311.jpg)] bg-cover bg-no-repeat"
+  >
     <HomeView />
     <AppNavbar />
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/skill.vue
+++ src/components/skill.vue
@@ -1,84 +1,67 @@
 <script>
-import "animate.css";
+import 'animate.css';
 </script>
 
 <template>
   <div
-    class="absolute w-[100%] flex pb-10 top-0 justify-center modalBg animate__animated animate__backInUp overflow-hidden "
+    class="absolute w-[100%] flex pb-10 top-0 justify-center modalBg animate__animated animate__backInUp overflow-hidden h-[100vh]"
   >
-    <div class="w-[300px] lg:w-[500px] p-10 cardBg mt-2  ">
+    <div class="w-[300px] lg:w-[500px] p-10 cardBg mt-2">
       <div class="text-center text-4xl font-bold">skill</div>
-      <div class="flex w-[100%] h-10 leading-4 space-x-2">
-        <div class="w-20">
-          <img
-            src="../assets/html5.svg"
-            class="animate__animated animate__pulse animate__infinite w-8 h-10"
-          />
-          <div class="text-lg uppercase font-bold">html5</div>
+      <div class="grid grid-cols-2 lg:md:grid-cols-3 gap-4">
+        <div class="flex w-[100%] h-10 leading-4 space-x-2 mt-5">
+          <div class="w-20">
+            <img
+              src="../assets/html5.svg"
+              class="animate__animated animate__pulse animate__infinite w-8 h-10"
+            />
+            <div class="text-lg uppercase font-bold">html5</div>
+          </div>
         </div>
-
-        <div class="skill-level rounded-md w-[100%] bg-gray h-2 mt-2">
-          <div class="skill-percent w-[90%] bg-blue1 h-2 rounded-md"></div>
+        <div class="flex w-[100%] h-10 leading-4 mt-5 space-x-2">
+          <div class="w-20">
+            <img
+              src="../assets/css3-alt.svg"
+              class="animate__animated animate__pulse animate__infinite w-10 h-10"
+            />
+            <div class="text-lg uppercase font-bold">css3</div>
+          </div>
         </div>
-      </div>
-      <div class="flex w-[100%] h-10 leading-4 mt-10 space-x-2">
-        <div class="w-20">
-          <img
-            src="../assets/css3-alt.svg"
-            class="animate__animated animate__pulse animate__infinite w-10 h-10"
-          />
-          <div class="text-lg uppercase font-bold">css3</div>
+        <div class="flex w-[100%] h-10 leading-4 mt-5 space-x-2">
+          <div class="w-20">
+            <img
+              src="../assets/square-js.svg"
+              class="animate__animated animate__pulse animate__infinite w-10 h-10"
+            />
+            <div class="text-lg uppercase font-bold">js</div>
+          </div>
         </div>
-        <div class="skill-level rounded-md w-[100%] bg-gray h-2 mt-5">
-          <div class="skill-percent w-[85%] bg-blue1 h-2 rounded-md"></div>
+        <div class="flex w-[100%] h-10 leading-4 mt-5 space-x-2">
+          <div class="w-20">
+            <img
+              src="../assets/bootstrap.svg"
+              class="animate__animated animate__pulse animate__infinite w-10 h-10"
+            />
+            <div class="text-lg uppercase font-bold">bootstrap</div>
+          </div>
         </div>
-      </div>
-      <div class="flex w-[100%] h-10 leading-4 mt-10 space-x-2">
-        <div class="w-20">
-          <img
-            src="../assets/square-js.svg"
-            class="animate__animated animate__pulse animate__infinite w-10 h-10"
-          />
-          <div class="text-lg uppercase font-bold">js</div>
+        <div class="flex w-[100%] h-10 leading-4 mt-5 space-x-2">
+          <div class="w-20">
+            <img
+              src="../assets/tailwind-css.svg"
+              class="animate__animated animate__pulse animate__infinite w-10 h-10"
+            />
+            <div class="text-lg uppercase font-bold">tailwind</div>
+          </div>
         </div>
-        <div class="skill-level rounded-md w-[100%] bg-gray h-2 mt-2">
-          <div class="skill-percent w-[80%] bg-blue1 h-2 rounded-md"></div>
-        </div>
-      </div>
-      <div class="flex w-[100%] h-10 leading-4 mt-10 space-x-2">
-        <div class="w-20">
-          <img
-            src="../assets/bootstrap.svg"
-            class="animate__animated animate__pulse animate__infinite w-10 h-10"
-          />
-          <div class="text-lg uppercase font-bold">bootstrap</div>
-        </div>
-        <div class="skill-level rounded-md w-[100%] bg-gray h-2 mt-2">
-          <div class="skill-percent w-[70%] bg-blue1 h-2 rounded-md"></div>
-        </div>
-      </div>
-      <div class="flex w-[100%] h-10 leading-4 mt-10 space-x-2">
-        <div class="w-20">
-          <img
-            src="../assets/tailwind-css.svg"
-            class="animate__animated animate__pulse animate__infinite w-10 h-10"
-          />
-          <div class="text-lg uppercase font-bold">tailwind</div>
-        </div>
-        <div class="skill-level rounded-md w-[100%] bg-gray h-2 mt-2">
-          <div class="skill-percent w-[80%] bg-blue1 h-2 rounded-md"></div>
-        </div>
-      </div>
-      <div class="flex w-[100%] h-10 leading-4 mt-10 space-x-2">
-        <div class="w-20">
-          <img
-            src="../assets/vuejs.svg"
-            class="animate__animated animate__pulse animate__infinite w-10 h-10"
-          />
-          <div class="text-lg uppercase font-bold">vue</div>
-        </div>
-        <div class="skill-level rounded-md w-[100%] bg-gray h-2 mt-2">
-          <div class="skill-percent w-[80%] bg-blue1 h-2 rounded-md"></div>
+        <div class="flex w-[100%] h-10 leading-4 mt-5 space-x-2">
+          <div class="w-20">
+            <img
+              src="../assets/vuejs.svg"
+              class="animate__animated animate__pulse animate__infinite w-10 h-10"
+            />
+            <div class="text-lg uppercase font-bold">vue</div>
+          </div>
         </div>
       </div>
     </div>
